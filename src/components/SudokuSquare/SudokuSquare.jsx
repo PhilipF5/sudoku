d996cb5b7d9b590ab077453dd355e9958c75ec94
Philip Fulgham <philip@philipf5.dev>
2019-06-30 21:05:30 -0400

    Refactor context position handling

--- src/components/SudokuSquare/SudokuSquare.jsx
+++ src/components/SudokuSquare/SudokuSquare.jsx
@@ -3,15 +3,14 @@ import { PuzzleContext } from "../../context/PuzzleContext";
 import styles from "./SudokuSquare.module.css";
 
 const SudokuSquare = ({ index }) => {
-	const context = useContext(PuzzleContext);
-	const row = context.rowOfSquare(index);
-	const column = context.columnOfSquare(index);
-	const isSelected = context.selectedSquare === index;
+	const { position, selectSquare, selectedSquare } = useContext(PuzzleContext);
+	const { column, row } = position(index);
+	const isSelected = selectedSquare === index;
+	const classNames = [styles.square, (row + 1) % 3 === 0 ? styles.thirdRow : "", isSelected ? styles.selected : ""];
 	const handleClick = () => {
 		console.log(`Square #${index} clicked! Row ${row}, Column ${column}`);
-		context.selectSquare(index);
+		selectSquare(index);
 	};
-	const classNames = [styles.square, (row + 1) % 3 === 0 ? styles.thirdRow : "", isSelected ? styles.selected : ""];
 	return <div className={classNames.join(" ")} onClick={handleClick}></div>;
 };
 
--- src/context/PuzzleContext.js
+++ src/context/PuzzleContext.js
@@ -3,8 +3,7 @@ import { createContext } from "react";
 export const initialState = {
 	grid: Array.apply(null, Array(81)),
 	selectedSquare: null,
-	rowOfSquare: square => Math.trunc(square / 9),
-	columnOfSquare: square => square % 9,
+	position: square => ({ column: square % 9, row: Math.trunc(square / 9) }),
 	setSquare: (grid, index, value) => (grid[index] = value),
 	row: (grid, index) => grid.slice(index * 9, (index + 1) * 9),
 	column: (grid, index) => grid.filter((_, square) => square % 9 === index % 9),
