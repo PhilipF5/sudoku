633e16eb7d7d3419125c206edef2c4d4bffcb47d
Philip Fulgham <philip@philipf5.dev>
2019-07-04 16:44:59 -0400

    Keep squares enlarged when selected

--- src/components/SudokuGrid/SudokuGrid.jsx
+++ src/components/SudokuGrid/SudokuGrid.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import SudokuSquare from "../SudokuSquare/SudokuSquare";
 import styles from "./SudokuGrid.module.css";
 
-const SudokuGrid = ({ grid, selectSquare, selectedSquare }) => {
-	const position = index => ({ column: columnOfSquare(index), grid: gridOfSquare(index), row: rowOfSquare(index) });
-	return (
-		<div className={styles.grid}>
-			{grid.map((value, index) => (
-				<SudokuSquare
-					{...{ index, value, selectSquare }}
-					key={index}
-					selected={selectedSquare === index}
-					position={position(index)}
-				/>
-			))}
-		</div>
-	);
-};
+const SudokuGrid = ({ grid, selectSquare, selectedSquare }) => (
+	<div className={styles.grid}>
+		{grid.map((value, index) => (
+			<SudokuSquare
+				{...{ index, value, selectSquare }}
+				key={index}
+				selected={selectedSquare === index}
+				position={position(index)}
+			/>
+		))}
+	</div>
+);
 
 const columnOfSquare = index => index % 9;
 const rowOfSquare = index => Math.trunc(index / 9);
 const gridOfSquare = index => Math.trunc(columnOfSquare(index) / 3) + Math.trunc(rowOfSquare(index) / 3) * 3;
+const position = index => ({ column: columnOfSquare(index), grid: gridOfSquare(index), row: rowOfSquare(index) });
 
 export default SudokuGrid;
