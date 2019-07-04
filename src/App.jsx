83bec9ce82915f95e708ccc8b7264809f9da21c9
Philip Fulgham <philip@philipf5.dev>
2019-07-04 10:46:36 -0400

    Refactor puzzle creation to prevent incessant re-generation of puzzles on render

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
 import { SudokuSolver } from "@jlguenego/sudoku-generator";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import NumberPicker from "./components/NumberPicker/NumberPicker";
 import SudokuGrid from "./components/SudokuGrid/SudokuGrid";
 
+const difficulty = {
+	easy: 47,
+	medium: 51,
+	hard: 55,
+};
+
 const App = () => {
-	const completed = SudokuSolver.generate();
-	const start = SudokuSolver.carve(completed, 55)
-		.flat()
-		.map(value => value || null);
-	const [grid, setGrid] = useState(start);
+	const [grid, setGrid] = useState(Array.apply(null, Array(81)));
+	const [solution, setSolution] = useState(Array.apply(null, Array(81)));
 	const [selectedSquare, selectSquare] = useState(null);
 	const setSquare = value => {
 		const newGrid = [...grid];
 		newGrid[selectedSquare] = value;
 		setGrid(newGrid);
 	};
+
+	useEffect(() => {
+		const { completed, start } = newPuzzle();
+		setGrid(start);
+		setSolution(completed);
+	}, []);
+
 	return (
 		<div className="App">
 			<SudokuGrid grid={grid} selectSquare={selectSquare} selectedSquare={selectedSquare} />
 			<NumberPicker setSquare={setSquare} />
 		</div>
 	);
+};
+
+const newPuzzle = (level = "easy") => {
+	const completed = SudokuSolver.generate();
+	return {
+		completed,
+		start: SudokuSolver.carve(completed, difficulty[level])
+			.flat()
+			.map(value => value || null),
+	};
 };
 
 export default App;
