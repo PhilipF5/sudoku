67a883b9d897b4dee76987fbffb2578c535b68a4
Philip Fulgham <philip@philipf5.dev>
2019-07-05 13:53:49 -0400

    Refactor class names for squares

--- src/components/SudokuSquare/SudokuSquare.jsx
+++ src/components/SudokuSquare/SudokuSquare.jsx
@@ -3,7 +3,12 @@ import styles from "./SudokuSquare.module.css";
 
 const SudokuSquare = ({ index, value, selectSquare, selected, position }) => {
 	const { column, grid, row } = position;
-	const classNames = [styles.square, (row + 1) % 3 === 0 ? styles.thirdRow : "", selected ? styles.selected : ""];
+	const classNames = [
+		styles.square,
+		(column + 1) % 3 === 0 && column !== 8 && styles.thirdColumn,
+		(row + 1) % 3 === 0 && row !== 8 && styles.thirdRow,
+		selected && styles.selected,
+	].filter(c => !!c);
 	const handleClick = () => {
 		console.log(`Square #${index} clicked! Column ${column}, Row ${row}, Grid ${grid}`);
 		selectSquare(index);
