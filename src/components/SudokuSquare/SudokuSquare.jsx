a890fccd99aa3f1dec3ede90aa9614d9449cb398
Philip Fulgham <philip@philipf5.dev>
2019-11-04 19:26:22 -0500

    Deploy classnames library to replace homegrown alternative

--- src/components/SudokuSquare/SudokuSquare.jsx
+++ src/components/SudokuSquare/SudokuSquare.jsx
@@ -1,22 +1,26 @@
-import React, { useRef } from "react";
+import classNames from "classnames";
+import React, { useMemo, useRef } from "react";
 import styles from "./SudokuSquare.module.css";
 
 const SudokuSquare = ({ index, value, onSelect, selected, position }) => {
 	const { current: initialValue } = useRef(value);
 	const { column, grid, row } = position;
-	const classNames = [
-		styles.square,
-		(column + 1) % 3 === 0 && column !== 8 && styles.thirdColumn,
-		(row + 1) % 3 === 0 && row !== 8 && styles.thirdRow,
-		selected && styles.selected,
-		initialValue && styles.prefilled,
-	].filter((c) => !!c);
+	const inThirdColumn = useMemo(() => (column + 1) % 3 === 0 && column !== 8, [column]);
+	const inThirdRow = useMemo(() => (row + 1) % 3 === 0 && row !== 8, [row]);
 	const handleClick = () => {
 		console.log(`Square #${index} clicked! Column ${column}, Row ${row}, Grid ${grid}`);
 		onSelect(selected ? null : index);
 	};
 	return (
-		<div className={classNames.join(" ")} onClick={handleClick}>
+		<div
+			className={classNames(styles.square, {
+				[styles.thirdColumn]: inThirdColumn,
+				[styles.thirdRow]: inThirdRow,
+				[styles.selected]: selected,
+				[styles.prefilled]: initialValue,
+			})}
+			onClick={handleClick}
+		>
 			{value}
 		</div>
 	);
