59a86137078dde6c123e35c9cc3cef5158b04eaa
Philip Fulgham <philip@philipf5.dev>
2019-12-10 23:31:04 -0500

    Add Easter egg to portrait warning

--- src/components/PortraitWarning/PortraitWarning.jsx
+++ src/components/PortraitWarning/PortraitWarning.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styles from "./PortraitWarning.module.css";
 
 const PortraitWarning = () => {
-	return <div className={styles.portraitWarning}>Rotate your device to a landscape orientation to use this app.</div>;
+	const [clicks, setClicks] = useState(0);
+	const incrementClicks = useCallback(() => setClicks((c) => c + 1), [setClicks]);
+	const getMessage = useCallback(() => messages[Math.min(Math.floor(clicks / increment), messages.length - 1)], [
+		clicks,
+	]);
+	return (
+		<div className={styles.portraitWarning} onClick={incrementClicks}>
+			{getMessage()}
+		</div>
+	);
 };
+
+const increment = 5;
+
+const messages = [
+	"Rotate your device to a landscape orientation to use this app.",
+	"Sorry, this app just doesn't fit in portrait orientation.",
+	"Uh, hello? You need to rotate your device to use this app.",
+	"Rotate your device, you dolt!!!",
+];
 
 export default PortraitWarning;
